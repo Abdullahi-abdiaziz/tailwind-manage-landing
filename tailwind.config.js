ea519e65016fe31034f52058ad85fc878171810e
Abdullahi Abdiaziz yusuf <abdullahikawte2019@gmail.com>
2023-02-10 07:47:19 +0200

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: {
+    content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"]
+    // These options are passed through directly to PurgeCSS
+  },
  content: ["./src/**/*.{html,js}"],
   theme: {
     screen: {
