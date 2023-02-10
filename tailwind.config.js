a42db0ca4be2b8e92b78e3e31f5eafa4a92a12a6
Abdullahi Abdiaziz yusuf <abdullahikawte2019@gmail.com>
2023-02-10 08:08:51 +0200

    upd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"]
-    // These options are passed through directly to PurgeCSS
-  },
- content: ["./src/**/*.{html,js}"],
+content: ["./src/**/*.{html,js}"],
   theme: {
     screen: {
       sm: '480px',
